592657fb9fe2f575f3779deaf805ebf221e1add4
HeartUnchange <haoxiangzhao@outlook.com>
2017-03-13 11:01:51 +0800

    add before crawl

--- aiospider/spider.py
+++ aiospider/spider.py
@@ -6,8 +6,6 @@ main part
 import asyncio
 import logging
 import sys
-from functools import partial
-from urllib.parse import urljoin
 from itertools import zip_longest
 from collections import namedtuple
 
@@ -17,8 +15,9 @@ DEFAULT_HEADER = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537
                         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
                         }
 
+
 _Request = namedtuple("Request",["method","url","callback","header","data"])
-def Request(method,url,callback,header=DEFAULT_HEADER,data=None):
+def Request(method, url, callback, header=DEFAULT_HEADER, data=None):
     return _Request(method,url,callback,header,data)
 
 class Spider:
@@ -28,7 +27,7 @@ class Spider:
 
     default_config = {
         # How many requests can be run in parallel
-        "concurrent": 10,
+        "concurrent": 5,
         # How long to wait after each request
         "delay": 0,
         # A stream to where internal logs are sent, optional
@@ -42,18 +41,34 @@ class Spider:
 
         self.config.update(kwargs.get("config", {}))
 
-        self.loop = kwargs.get("loop", None)
+        self.loop = kwargs.get("loop", None) #if no loop, get one
         if self.loop is None or not isinstance(self.loop, asyncio.BaseEventLoop):
             self.loop = asyncio.get_event_loop()
+        '''
+        if no session , new one.
+        Providing a session is convenient when you spider some that you need to login, you can just pass a logged-in session.
+        Of course, you can provide a function which will be call before all spider requests to log in.
+        '''
         self.session = kwargs.get("session", None)
         if self.session is None or not isinstance(self.session, aiohttp.ClientSession):
             self.session = aiohttp.ClientSession(loop=self.loop)
+
+        '''
+         The methods contained here will be called before any requests.
+         For example,if spider need to login, you may need provide login method.
+        '''
+        self.before_start_funcs = []
+        self.will_continue = True
+        '''
+         The methods contained here will be called after all requests.
+         For example,if spider need to logout, you may need provide logout method.
+        '''
+        self.after_crawl_funcs = []
 
         self.logger = logging.getLogger(self.__class__.__name__)
         self.pending = asyncio.Queue()
         self.active = []
         self.visited = set()
-        pass
 
     def __enter__(self):
         return self
@@ -68,6 +83,14 @@ class Spider:
         self.logger.warning(status + " " + url)
 
     def add_request(self, url, callback, method="GET", **kwargs):
+        '''
+         Add request wo queue.
+        :param url: request's url
+        :param callback: which will be called after request finished.
+        :param method: request's method
+        :param kwargs: additional parameters for request
+        :return: None
+        '''
         if url in self.visited:
             return
         if not self.config["allowDuplicates"]:
@@ -76,21 +99,50 @@ class Spider:
         self.pending.put_nowait(request)
         self.log("ADD", url)
 
+    def before_start(self,func):
+        '''
+        add function called b
+        :param func:
+        :return:
+        '''
+        self.before_start_funcs.append(func)
+        return func
+    def after_spider(self,func):
+        self.after_crawl_funcs.append(func)
+        return func
+
+    async def try_trigger_before_start_functions(self):
+        for func in self.before_start_funcs:
+            if callable(func) :
+                if asyncio.iscoroutinefunction(func):
+                    await func(self)
+                else:
+                    func(self)
+    async def try_trigger_after_crawl_functions(self):
+        for func in self.after_crawl_funcs:
+            if callable(func) :
+                if asyncio.iscoroutinefunction(func):
+                    await func(self)
+                else:
+                    func(self)
     async def load(self):
+        '''
+         pop request from queue to send
+        :return:
+        '''
         try:
             while True:
                 request = await self.pending.get()
                 self.log("Loading", request.url)
-                await self.__request(request)
+                await self.request_with_callback(request)
                 self.pending.task_done()
         except asyncio.CancelledError:
             pass
 
-    async def __request(self, request: _Request):
-        #print("request url: %s"%request.url)
+    async def request_with_callback(self, request: _Request):
         async with self.session.request(request.method, request.url) as resp:
-            # self.log("Parse",resp.url)
             if callable(request.callback) and asyncio.iscoroutinefunction(request.callback):
+                # loop.call_later better?
                 await request.callback(resp)
 
     async def download(self, src, dst):
@@ -115,4 +167,8 @@ class Spider:
     def start(self, urls, callbacks):
         for url, callback in zip_longest(urls, callbacks, fillvalue=callbacks[len(callbacks) - 1]):
             self.add_request(url, callback)
-        self.loop.run_until_complete(self.__start())
+
+        self.loop.run_until_complete(self.try_trigger_before_start_functions())
+        if self.will_continue :
+            self.loop.run_until_complete(self.__start())
+        self.loop.run_until_complete(self.try_trigger_after_crawl_functions())
---
+++ examples/ex3.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+#-*-coding:utf8-*-
+
+import aiohttp
+from aiospider import Spider
+from functools import partial
+from pyquery import PyQuery
+
+ZHIHU_INDEX = "https://www.zhihu.com/"
+ZHIHU_LOGIN = "https://www.zhihu.com/login/email"
+
+with Spider() as ss:
+    @ss.before_start
+    async def login(spider):
+        # not suitable, will changed later
+        async with spider.session.request("get", ZHIHU_INDEX) as resp:
+            pq = PyQuery(await resp.text())
+            _xsrf = PyQuery(pq('input[type="hidden"][name="_xsrf"]')[0]).attr("value")
+        password = ""
+        email = ""
+        async with spider.session.request("POST", ZHIHU_LOGIN, data={"email": email, "password": password, "_xsrf": _xsrf}) as resp:
+            js = await resp.json()
+            if not js.get("r", 1):
+                print(js.get("msg", ""))
+            else:
+                spider.will_continue = False
+                print(js.get("msg", ""))
+
+    async def logged(response: aiohttp.Response):
+        print(response.url)
+    ss.start(["https://www.zhihu.com/people/edit", ], [logged, ])
